fd1551f5758ff76bd245f4ab195164ab753b0b19
Дмитрий Мазин <mitya.mazin@mail.ru>
2023-10-16 16:27:48 +0300

    Add project files.

---
+++ WebProductCategory/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebProductCategory.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace WebProductCategory.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ApplicationDbContext context, ILogger<ProductController> logger)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult GetProductsAndCategories()
+        {
+            var productsWithCategories = _context.Products.Include(p => p.Category).ToList();
+            return View(productsWithCategories);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebProductCategory/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebProductCategory.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+            //Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
